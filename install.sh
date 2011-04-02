4730e651127e6bb6a1958d614dcdc53997e3b366
Patryk Rzucidlo <ptkdev@gmail.com>
2011-04-02 21:14:25 +0200

    test of @RoxShannon install.sh script - test

--- install.sh
+++ install.sh
@@ -1,1 +1,14 @@
-// Coming Soon
+#!/bin/bash
+
+liboauth="liboauth-0.9.4.tar.gz"
+link="http://downloads.sourceforge.net/project/liboauth/liboauth-0.9.4.tar.gz"
+mkdir -p opensource
+cd opensource
+wget $link
+tar xvvf $liboauth
+cd liboauth-0.9.4
+./configure
+make
+make install
+cd ../../src
+gcc -Wall -lssl -loauth -o twc twc.c oauth.c credits.c other.c setting.c statusbar.c `pkg-config --cflags --libs gtk+-2.0`
