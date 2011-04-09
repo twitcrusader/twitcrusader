b04a383aca31e344e78648d5302246a455a517a8
Patryk Rzucidlo <ptkdev@gmail.com>
2011-04-09 18:59:56 +0200

    remove website folder, new repositor coming soon

--- website/wp-theme-it/desktop.php
+++
@@ -1,5 +0,0 @@
-	<div id="content">	
-		<img src="<?php bloginfo('template_url'); ?>/img/desktop/twc.png" alt="TwitCrusader" title="TwitCrusader" /><br />
-		<div class="clear"></div>
-	</div>
-</div>
--- website/wp-theme-it/footer-mobile.php
+++
@@ -1,8 +0,0 @@
-	<div id="sep"></div>
-	<div id="footer">
-		TwitCrusader ~ Version Mobile 0.1<br />
-		2011 ~ <?php echo date('Y')+1;?>
-	</div>
-<?php wp_footer(); ?>
-</body>
-</html>
