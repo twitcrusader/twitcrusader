9a23cc070dd38b120f6029787e1022133574cbf1
Orazio <orazio.1985@hotmail.com>
2011-04-26 14:31:42 +0200

    Imega Download is OK

--- src/gethttp.c
+++ src/gethttp.c
@@ -1,9 +1,26 @@
 /*
- * getimage.c
- *
- *  Created on: Apr 26, 2011
- *      Author: darkstar
- */
+*	TwitCrusader - Twitter Client For Linux Desktop
+*		Copyright (C) 2011  PTKDev, RoxShannon
+*
+*		This program is free software: you can redistribute it and/or modify
+*		it under the terms of the GNU General Public License as published by
+*		the Free Software Foundation, either version 3 of the License, or
+*		(at your option) any later version.
+*
+*		This program is distributed in the hope that it will be useful,
+*		but WITHOUT ANY WARRANTY; without even the implied warranty of
+*		MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*		GNU General Public License for more details.
+*
+*		You should have received a copy of the GNU General Public License
+*		along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+*
+*		Author: Patryk Rzucidlo (PTKDev)
+*		Twitter: @ptkdev / @twitcrusader_en
+*		WebSite: http://www.twitcrusader.org
+*/
+
 #include "include/gethttp.h"
 
 size_t writeFunction( void *ptr, size_t size, size_t nmemb, void *stream){
