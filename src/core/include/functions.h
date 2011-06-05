6d7ff558ef655da36d072d900c36f0f8413da915
Patryk Rzucidlo <ptkdev@gmail.com>
2011-06-05 20:43:21 +0200

    Rebased Nightly Branch

---
+++ src/core/include/functions.h
@@ -0,0 +1,42 @@
+/*
+ *	TwitCrusader - Twitter Client For Linux Desktop
+ *		Copyright (C) 2011  PTKDev, RoxShannon
+ *
+ *		This program is free software: you can redistribute it and/or modify
+ *		it under the terms of the GNU General Public License as published by
+ *		the Free Software Foundation, either version 3 of the License, or
+ *		(at your option) any later version.
+ *
+ *		This program is distributed in the hope that it will be useful,
+ *		but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *		MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *		GNU General Public License for more details.
+ *
+ *		You should have received a copy of the GNU General Public License
+ *		along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ *		Author: Patryk Rzucidlo (PTKDev)
+ *		Twitter: @ptkdev / @twitcrusader_en
+ *		WebSite: http://www.twitcrusader.org
+ * 		IRC: chat.freenode.net at #teamtwc
+ */
+
+#ifndef FUNCTION_H_
+#define FUNCTION_H_
+
+#define _GNU_SOURCE
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int debug;
+void mollocSizeOF();
+void freeSizeOF(void);
+char *sumStrings(char *parm1, char *parm2 );
+char* getParameters(char** argv, int argc, const char* param);
+int shellParameters (int argc, char **argv);
+void createDir();
+
+#endif /* FUNCTION_H_ */
