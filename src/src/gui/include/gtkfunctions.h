b4de83e166a2012a033dceb41de24dfbab60a530
Patryk Rzucidlo <ptkdev@gmail.com>
2011-06-05 14:42:33 +0200

    Testing: ReEngine Of Code from «test» branch @RoxShannon

---
+++ src/src/gui/include/gtkfunctions.h
@@ -0,0 +1,77 @@
+/*
+ *	TwitCrusader - Twitter Client For Linux Desktop
+ *		Copyright (C) 2011  PTKDev, RoxShannon
+ *
+ *		This program is free software: you can redistribute it and/or modify
+ *		it under the terms of the GNU General Public License as published by
+ *		the Free Software Foundation, either version 3 of the License, or
+ *		(at your option) any later version.
+ *
+ *		This program is distributed in the hope that it will be useful,
+ *		but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *		MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *		GNU General Public License for more details.
+ *
+ *		You should have received a copy of the GNU General Public License
+ *		along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ *		Author: Patryk Rzucidlo (PTKDev)
+ *		Twitter: @ptkdev / @twitcrusader_en
+ *		WebSite: http://www.twitcrusader.org
+ * 		IRC: chat.freenode.net at #teamtwc
+ */
+
+#ifndef GTKWINDOWS_H_
+#define GTKWINDOWS_H_
+
+#define _GNU_SOURCE
+
+#include <stdio.h>
+#include <gtk/gtk.h>
+#include <glib.h>
+#include <pthread.h>
+#include <gdk/gdkkeysyms.h>
+
+#include <string.h>
+#include <errno.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+#include "windows.h"
+
+#include "../../core/include/functions.h"
+#include "../../core/include/twitter.h"
+#include "../../core/include/gethttp.h"
+
+typedef struct
+{
+	GtkWidget *pin;
+	GtkWidget *window;
+
+} AuthWidget;
+
+struct StatusBars
+{
+	GtkStatusbar *message;
+
+} StatusBar;
+
+typedef struct configuration {
+	char* default_user;
+}config;
+
+void destroyGtk(GtkButton *button, gpointer widget);
+void updateGtk(GtkButton *button, gpointer window);
+gboolean gtkOnKeyPress (GtkWidget * window, GdkEventKey* pKey, gpointer userdata);
+gboolean gtkSendTweet(GtkWidget *TextArea, GdkEventKey *pKey, GtkTextBuffer *tweetBuffer);
+void updateStatusBar(GtkTextBuffer *buffer, GtkStatusbar  *statusbar);
+void gtkSwitchPage (GtkButton *button, GtkNotebook *notebook);
+void gtkAccessToken(GtkButton *button, AuthWidget *DataInput);
+void gtkDeleteAccount(GtkButton *button, gpointer widget);
+void gtkConnect(GtkButton *button, gpointer window);
+void gtkDisconnect(GtkButton *button, gpointer window);
+void gtkAddUser(GtkButton *button, gpointer window);
+void gtkRefreshswitchTimeLine(GtkWidget *table_into, gpointer window);
+
+#endif /* GTKWINDOWS_H_ */
