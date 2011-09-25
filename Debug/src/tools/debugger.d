70c071a352f927bc753647aa79daf94345c2ab70
PTKDev <ptkdev@gmail.com>
2011-09-25 18:47:18 +0200

    Fix RoxShannon Merge

--- Debug/src/tools/debugger.d
+++
@@ -1,3 +0,0 @@
-src/tools/debugger.d: ../src/tools/debugger.c ../src/tools/inc/debugger.h
-
-../src/tools/inc/debugger.h:
--- src/main.c
+++ src/main.c
@@ -38,6 +38,10 @@
  */
 int main(int argc, char *argv[]){
 
+	bindtextdomain(GETTEXT_PACKAGE, PACKAGE_LOCALE_DIR);
+	bind_textdomain_codeset(GETTEXT_PACKAGE, "UTF-8");
+	textdomain(GETTEXT_PACKAGE);
+
 	notify_init(TWC);
 
 	gtk_init (&argc, &argv);
